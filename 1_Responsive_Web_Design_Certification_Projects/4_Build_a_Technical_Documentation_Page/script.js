af8c6a86d268b38657eb223bae10e0676b9bd5f3
n4d114-k <n4d114.k@gmail.com>
2020-10-26 17:33:12 +0200

    end tech documentation page

---
+++ 1_Responsive_Web_Design_Certification_Projects/4_Build_a_Technical_Documentation_Page/script.js
@@ -0,0 +1,17 @@
+function mOver(obj) {
+  obj.innerHTML = "Thank You"
+}
+
+function mOut(obj) {
+  obj.innerHTML = "Mouse Over Me"
+}
+
+function mDown(obj) {
+  obj.style.backgroundColor = "#1ec5e5";
+  obj.innerHTML = "Release Me";
+}
+
+function mUp(obj) {
+  obj.style.backgroundColor="#dc143c";
+  obj.innerHTML="Thank You";
+}
